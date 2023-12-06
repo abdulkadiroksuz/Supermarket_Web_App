3f8c0d786b5d4a3d2c581f0997f46844f2a4ca75
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-12-06 19:46:04 +0300

    Visualize Index
    Changing opacity by scrolling down

---
+++ market/core/static/index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var homeBanner = document.getElementById("home-banner");
+
+    function handleScroll() {
+        var scrollY = window.scrollY || window.pageYOffset;
+        var scrollThreshold = 300; // Adjust this value based on when you want the effect to start
+
+        if (scrollY > scrollThreshold) {
+            homeBanner.classList.add("banner-hidden");
+        } else {
+            homeBanner.classList.remove("banner-hidden");
+        }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+});
