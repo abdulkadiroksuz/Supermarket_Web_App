93efbc78b6fb22dc6c4d6f3f5f99477158c09667
eren <eren.cicek62@gmail.com>
2023-12-07 21:38:17 +0300

    base.html is deleted and created in new templates folder

--- market/market/settings.py
+++ market/market/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,7 +68,9 @@ ROOT_URLCONF = 'market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,3 +139,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.DEBUG : "alert-secondary",
+    messages.INFO : "alert-info",
+    messages.SUCCESS : "alert-success",
+    messages.WARNING : "alert-warning",
+    messages.ERROR : "alert-danger",
+}
