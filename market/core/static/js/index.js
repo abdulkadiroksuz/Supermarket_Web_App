4a8f883970d314e56844c9b64d18a1027f30641b
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-12-07 20:53:19 +0300

    Slide Effect for Login-Form

--- market/core/static/js/index.js
+++ market/core/static/js/index.js
@@ -14,3 +14,35 @@ document.addEventListener("DOMContentLoaded", function() {
 
     window.addEventListener("scroll", handleScroll);
 });
+
+
+let login = document.querySelector('.login-form');
+
+document.querySelector('#user-icon').onclick = () =>{
+  login.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  login.classList.remove('active');
+}
+
+// var swiper = new Swiper(".review-slider", {
+//   spaceBetween:20,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 7500,
+//     disableOnInteraction: false,
+//   },
+//   loop: true,
+//   breakpoints: {
+//     0:{
+//       slidesPerView: 1,
+//     },
+//     768:{
+//       slidesPerView: 2,
+//     },
+//     991:{
+//       slidesPerView: 3,
+//     },
+//   },
+// });
