38553b1a1fd89c4c0a5cc831320a405940c48073
Gürkan Bıyık <gurkanbiyik00@gmail.com>
2023-12-08 11:19:22 +0300

    Update: base.css, index.js, layout.html
    -add remember me functionality
    -adjust login form right and top

--- market/core/static/js/index.js
+++ market/core/static/js/index.js
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 let login = document.querySelector('.login-form');
+let username_holder =
 
 document.querySelector('#user-icon').onclick = () =>{
   login.classList.toggle('active');
@@ -26,23 +27,43 @@ window.onscroll = () =>{
   login.classList.remove('active');
 }
 
-// var swiper = new Swiper(".review-slider", {
-//   spaceBetween:20,
-//   centeredSlides: true,
-//   autoplay: {
-//     delay: 7500,
-//     disableOnInteraction: false,
-//   },
-//   loop: true,
-//   breakpoints: {
-//     0:{
-//       slidesPerView: 1,
-//     },
-//     768:{
-//       slidesPerView: 2,
-//     },
-//     991:{
-//       slidesPerView: 3,
-//     },
-//   },
-// });
+
+
+let usernameInput = document.querySelector('input[name="username"]');
+let passwordInput = document.querySelector('input[name="password"]');
+let rememberCheckbox = document.getElementById('remember-me');
+
+function saveCredentials() {
+  if (rememberCheckbox.checked) {
+    // If the "Remember me" checkbox is checked, save the credentials in cookies
+    document.cookie = `savedUsername=${encodeURIComponent(usernameInput.value)};`;
+    document.cookie = `savedPassword=${encodeURIComponent(passwordInput.value)};`;
+  } else {
+    // If not checked, clear any saved credentials by setting expiration in the past
+    document.cookie = 'savedUsername=; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+    document.cookie = 'savedPassword=; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+  }
+}
+
+// Function to get the value of a cookie by its name
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  
+  if (parts.length === 2) {
+    return parts.pop().split(';').shift();
+  }
+}
+
+window.onload = function() {
+  // When the page loads, check the saved credentials if any
+  let savedUsername = getCookie('savedUsername');
+  let savedPassword = getCookie('savedPassword');
+  
+  if (savedUsername && savedPassword) {
+    // If the saved credentials are valid, fill in the form fields
+    usernameInput.value = decodeURIComponent(savedUsername);
+    passwordInput.value = decodeURIComponent(savedPassword);
+    rememberCheckbox.checked = true;
+  }
+}
