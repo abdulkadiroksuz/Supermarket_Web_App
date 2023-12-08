6a30c498306525cc06649de3f3da982eb97039e2
Gürkan Bıyık <gurkanbiyik00@gmail.com>
2023-12-08 15:47:12 +0300

    Refactor cart.html and views.py for improved readability and performance

--- market/cart/views.py
+++ market/cart/views.py
@@ -1,11 +1,9 @@
-from django.db import connection
-from django.db.models.fields.files import ImageFieldFile
-from market.settings import MEDIA_URL
 from django.shortcuts import render
+from market.settings import MEDIA_URL
 from item.models import Category
 from user.models import Customer
 
-from .models import Cart
+from .models import Cart,CartProduct
 
 
 # Create your views here.
@@ -16,42 +14,24 @@ def cart(request):
         cart = Cart.objects.get(customer=customer)
     except Cart.DoesNotExist:
         cart = Cart.objects.create(customer=customer)
-    cart = cart.id
 
-    raw_sql_query = (
-        "SELECT p.*, c.quantity "
-        + "FROM cart_cartproduct as c "
-        + "INNER JOIN item_product as p "
-        + " ON c.product_id = p.id "
-        + "WHERE c.cart_id = "
-        + str(
-            cart,
+    cart_products = (
+        CartProduct.objects.filter(cart=cart)
+        .select_related("product")
+        .values(
+            "product__id",
+            "product__name",
+            "product__price",
+            "product__description",
+            "product__slug",
+            "product__image",
+            "quantity",
         )
-        + ";"
     )
 
-    with connection.cursor() as cursor:
-        cursor.execute(raw_sql_query)
-        results = cursor.fetchall()
-
-    # Convert results to dictionary by dict comprehension
-    results_dict = [
-        {
-            "product": {
-                "id": x[0],
-                "name": x[1],
-                "price": x[2],
-                "description": x[3],
-                "slug": x[5],
-                "image": "/media/"+x[6],
-            },
-            "quantity": x[7],
-        }
-        for x in results
-    ]
     context = {
         "categories": categories,
-        "items": results_dict,
-        "total_items": len(results),
+        "items": cart_products,
+        "total_items": len(cart_products),
     }
     return render(request, "cart.html", context)
