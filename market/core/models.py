4a0ba1ce02d9706f426f3ec1fcddfdfe06ed1c9d
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-12-11 00:29:23 +0300

    Add model Company

--- market/core/models.py
+++ market/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
