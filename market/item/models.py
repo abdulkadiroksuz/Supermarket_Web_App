384f2d9138851f984e99953a543ccc6d5607da5c
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-12-11 00:30:55 +0300

    Update Models
    Delete name field from storage. Add new table between product and category. So that one product can have multiple category. For example Egg.

--- market/item/models.py
+++ market/item/models.py
@@ -11,13 +11,25 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='product_images', blank=True, null=True)
-    price = models.IntegerField()
+    price = models.FloatField(null=False, blank=False)
     description = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+    
+    
+class ProductCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.category.name} - {self.product.name}"
+    
+    class Meta:
+        verbose_name_plural = "Product categories"
+        unique_together = ('category', 'product')
--- market/storage/models.py
+++ market/storage/models.py
@@ -1,27 +1,28 @@
 from django.db import models
-
-
+from django.core.validators import MinValueValidator
+from item.models import Product
+from core.models import Company
 # Create your models here.
-class Storage(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    area = models.ForeignKey('Area', on_delete=models.CASCADE)
+class Area(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
     
+class Storage(models.Model):
+    company = models.ForeignKey(Company, default=1 ,on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.company.name} - {self.area}"
+    
 class StorageProduct(models.Model):
     storage = models.ForeignKey(Storage, on_delete=models.CASCADE)
-    product = models.ForeignKey('item.Product', on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=100, validators=[MinValueValidator(0)])
     
     class Meta:
         unique_together = ('storage', 'product')
         
     def __str__(self):
         return f"{self.storage} - {self.product} - {self.quantity}"
-
-class Area(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
