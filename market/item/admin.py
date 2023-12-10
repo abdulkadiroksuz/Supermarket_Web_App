154b87d8659b495d98637abca40d3d7fd2b32a78
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-12-11 00:33:42 +0300

    Öyle işte
    set product-box width

--- market/item/admin.py
+++ market/item/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, ProductCategory
 # Register your models here.
+
 
 admin.site.register(Category)
 admin.site.register(Product)
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('category', 'product')
+admin.site.register(ProductCategory, ProductCategoryAdmin)
