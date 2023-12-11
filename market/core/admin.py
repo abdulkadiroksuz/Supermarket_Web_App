ad8279d815a08f88707dd4af744ecc11e8d1935a
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-12-11 14:14:00 +0300

    Remove Company model from admin panel
    Companies panel is unnecessary

--- market/core/admin.py
+++ market/core/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import Company
 
 # Register your models here.
-admin.site.register(Company)
+
+#admin.site.register(Company)
--- market/core/models.py
+++ market/core/models.py
@@ -8,3 +8,6 @@ class Company(models.Model):
     
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Companies'
