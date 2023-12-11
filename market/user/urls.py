001d98f0e60050f1c8dffa743be8ea918e22c3bc
Eren Çiçek <eren.cicek62@gmail.com>
2023-12-11 21:01:36 +0300

    öyle işte

--- market/user/urls.py
+++ market/user/urls.py
@@ -1,12 +1,11 @@
 # users_app/urls.py
 from django.urls import path
-from .views import loginView, signupView, profileView, logoutView
+from .views import loginView, signupView,logoutView
 
 app_name = 'user'
 
 urlpatterns = [   
     path('login/', loginView.as_view(), name='login'),
-    path('profile/', profileView, name='profile'),
     path('logout/', logoutView, name='logout'),
     path('signup/', signupView, name='signup'),
     # Add other URLs as needed
