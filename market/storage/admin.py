30075d018f2eaef51037b2ed7916f9d024d14ed9
Gürkan Bıyık <gurkanbiyik00@gmail.com>
2023-12-13 19:54:47 +0300

    admin panel storageproduct display

--- market/storage/admin.py
+++ market/storage/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Area, Storage, StorageProduct
+
 # Register your models here.
 admin.site.register(Area)
 admin.site.register(Storage)
-admin.site.register(StorageProduct)
 
 
+class StorageProductAdmin(admin.ModelAdmin):
+    list_display = ("product", "storage", "quantity")
+
+admin.site.register(StorageProduct, StorageProductAdmin)
