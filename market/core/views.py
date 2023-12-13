8a9faf3b0ba6d8d56453f41c9617a4e9cec1453d
Gürkan Bıyık <gurkanbiyik00@gmail.com>
2023-12-13 16:11:56 +0300

    #TODO popular products in index.html
    - after added some data to order_orderproducts table i will continue to implement

--- market/core/views.py
+++ market/core/views.py
@@ -1,15 +1,25 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from django.db.models import Q
-from item.models import Category
 from .models import Company
+from item.models import Category
 
 # Create your views here.
 def index(request):
     categories = Category.objects.all()
     
+     #  SQL QUERY ->
+#       SELECT p.*
+#       FROM item_product p
+#       JOIN (
+#           SELECT product_id, SUM(quantity) AS total
+#           FROM order_orderproduct
+#           GROUP BY product_id
+#       ) op ON p.id = op.product_id
+#       ORDER BY op.total DESC;
+    
     context = {
         'categories':categories,
+        # 'popular_products': popular_products[:12],
     }
     return render(request, 'core/index.html', context)
 
