8e3c48d80e4c78ea179e9026b0a2e4c170fb4fe8
Gürkan Bıyık <gurkanbiyik00@gmail.com>
2023-12-14 17:53:15 +0300

    fix add to cart

--- market/cart/urls.py
+++ market/cart/urls.py
@@ -6,7 +6,7 @@ app_name = 'cart'
 urlpatterns = [
     path("", cart, name="cart"),
     path("update_cart_item", update_product, name="update_product"),
-    path("delete_cart_item", delete_product, name="update_product"),
+    path("delete_cart_item", delete_product, name="delete_product"),
     path("update_navbar_cart", get_navbar_cart, name="update_navbar_cart"),
     path("add_item_to_cart", add_to_cart, name="add_item_to_cart"),
 ]
