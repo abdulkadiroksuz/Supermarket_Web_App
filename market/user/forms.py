f29d7ef3d9e989fee53233a3d8a37397897b8887
Eren Çiçek <eren.cicek62@gmail.com>
2023-12-14 15:49:08 +0300

    profile page is added but will be also add change_password

--- market/user/forms.py
+++ market/user/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
 from .models import Customer
 from storage.models import Area
@@ -77,3 +77,52 @@ class SignUpForm(UserCreationForm):
             customer.save()
         return user
    
+
+
+
+class ProfileUpdateForm(UserChangeForm):
+    phone = forms.CharField(
+        max_length=20, 
+        required=True, 
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'})
+    )
+    area = forms.ModelChoiceField(
+        queryset=Area.objects.all(), 
+        required=True, 
+        widget=forms.Select(attrs={'class': 'form-select', 'placeholder': 'Select an area'})
+    )
+    first_name = forms.CharField(
+        max_length=30, 
+        required=True, 
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'})
+    )
+    last_name = forms.CharField(
+        max_length=30, 
+        required=True, 
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'})
+    )
+    email = forms.EmailField(
+        max_length=254, 
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'})
+    )
+
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'phone', 'area')
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileUpdateForm, self).__init__(*args, **kwargs)
+        self.fields['area'].empty_label = '--select an area--'
+
+    def clean(self):      
+        email = self.cleaned_data.get('email')
+        phone = self.cleaned_data.get('phone')
+        user = self.instance
+        if email and User.objects.filter(email=email).exclude(pk=user.pk).exists():
+            self.add_error('email', 'This email is already in use.')
+
+        if phone and Customer.objects.filter(phone=phone).exclude(user=user).exists():
+            self.add_error('phone', 'This phone number is already in use.')
+
+        return self.cleaned_data
