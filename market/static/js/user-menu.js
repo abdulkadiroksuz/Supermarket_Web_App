8b41e8466b8a8d4dd4b98922dfb98418d4230ea3
Gürkan Bıyık <gurkanbiyik00@gmail.com>
2023-12-17 17:06:53 +0300

    fix hover affect on user-icon

--- market/static/js/user-menu.js
+++ market/static/js/user-menu.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
     $('#user-icon').click(function(event){
       // Toggle the display of the dropdown content
+      if ($('.user-dropdown-content').css('display') == 'none') {
+        $('#user-icon').css('border', '1px solid #ff6c57');
+      }else{
+        $('#user-icon').css('border', '0px solid #ff6c57');
+      }
+      
       $('.user-dropdown-content').slideToggle();
     });
 
@@ -8,11 +14,13 @@ $(document).ready(function(){
     $(document).click(function(event) {
       if (!$(event.target).closest('#user-icon').length && !$(event.target).hasClass('user-dropdown-content')) {
         $('.user-dropdown-content').slideUp();
+        $('#user-icon').css('border', '0px solid #ff6c57');
       }
     });
 
     // Prevent the dropdown from closing when clicking inside it
     $('.user-dropdown-content').click(function(event){
       event.stopPropagation();
+      $('#user-icon').css('border', '0px solid #ff6c57');
     });
   });
